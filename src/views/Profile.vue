8d75359e822378657d141d0cdfe7cb6c53a1223e
Zen Dharma <zendharma10@gmail.com>
2020-07-10 18:35:23 +0700

    Update Profile

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="profile">
-    <p>An Android developer, mainly, but sometimes also developing other stuffs.</p>
-    <p>Please go to Projects page to see what I have worked on.</p>
+    <p>Hello, the name is Zen.</p>
+    <p>Software developer, mainly for Android, but sometimes would tackle other platform as long as it's fun and would benefit people.</p>
+    <p>Please go to <a href="http://localhost:8080/projects">Projects</a> to see what I have worked on personally.</p>
+    <p>Visit my <a href="https://github.com/zend10" rel="noopener" target="_blank">GitHub</a> to see more stuffs I am developing.</p>
   </div>
 </template>
 
@@ -12,5 +14,7 @@ export default {
 </script>
 
 <style scoped lang="scss">
-
+a {
+  color: $secondary-color
+}
 </style>
